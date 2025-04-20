5f0a0a6b36e9df8d06a77557c36dc5f6155a6e2c
sjtrny <sjtrny@gmail.com>
2025-04-20 22:34:55 +1000

    Fix service name

--- monitor.py
+++ monitor.py
@@ -69,7 +69,7 @@ def handle_container_up(container_id):
 
             info = ServiceInfo(
                 type_="_http._tcp.local.",
-                name=f"{fqdn}._http._tcp.local.", # Use FQDN here for mDNS hostname resolution
+                name=f"{site_extract.subdomain}._http._tcp.local.", # Use subdomain here for service name
                 port=80,
                 addresses=[socket.inet_aton(PUBLISHED_IP)],
                 properties={},
